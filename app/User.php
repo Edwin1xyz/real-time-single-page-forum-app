eaf295fd04eff80e1ba593cce2707803397c985e
Edwin_Eddie <idongesitedidiong123@gmail.com>
2019-07-02 14:14:27 +0100

    Relationships

--- app/User.php
+++ app/User.php
@@ -2,6 +2,10 @@
 
 namespace App;
 
+use App\Model\Category;
+use App\Model\Like;
+use App\Model\Question;
+use App\Model\Reply;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -36,4 +40,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+
+    public function question(){
+        return $this->hasMany(Question::class);
+    }
+
+
 }
