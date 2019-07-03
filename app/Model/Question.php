e11a118ead4242c42745d8e59df19401fd9564a7
Edwin_Eddie <idongesitedidiong123@gmail.com>
2019-07-04 00:06:32 +0100

    CRUD for question along with Question Resource Transformation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,6 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+//    protected $fillable = ['title','slug','body','category_id','user_id'];
+
+    // the above format might not be the best way to do that when you have over 20 fields to fill,and you shouldn;t have to fill the box with all 20 field name,below is the best way to solve this problem (protected $guarded = [])
+
+    protected $guarded = [];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
@@ -17,5 +28,10 @@ class Question extends Model
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+
+    public function getPathAttribute(){
+      return asset("api/question/$this->slug");
     }
 }
