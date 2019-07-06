025cb7dcc567fe5bdaf29c80669c8efca7c8a685
Edwin_Eddie <idongesitedidiong123@gmail.com>
2019-07-06 12:19:58 +0100

    like and unlike

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers;
 use App\Model\Like;
 use App\Model\Reply;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class LikeController extends Controller
 {
     public function likeIt(Reply $reply){
-
+        $reply->like()->create([
+//            'user_id' => auth()->id,
+            'user_id' => '1',
+        ]);
+        return response('liked', Response::HTTP_OK);
     }
 
 
     public function unLikeIt(Reply $reply){
-
+//        $reply->like()->where(['user_id' => auth()->id() ])->first()->delete();
+        $reply->like()->where('user_id', 1)->first()->delete();
+        return response('unliked', Response::HTTP_OK);
     }
 }
