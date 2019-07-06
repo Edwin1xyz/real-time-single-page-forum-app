355911b7eb0f7aae431751f0d2a9eb3912db94f0
Edwin_Eddie <idongesitedidiong123@gmail.com>
2019-07-06 13:34:55 +0100

    JWT

--- routes/api.php
+++ routes/api.php
@@ -27,5 +27,19 @@ Route::post('/like/{reply}', 'LikeController@likeIt');
 
 Route::delete('/like/{reply}', 'LikeController@unLikeit');
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
 
 
