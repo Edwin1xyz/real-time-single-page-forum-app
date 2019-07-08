6ad38ba82e87e8b1ff911557a445cbbea7748bde
Edwin_Eddie <idongesitedidiong123@gmail.com>
2019-07-08 15:25:51 +0100

    Vuetify,Vue Router and Initial Components

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,7 +24,7 @@ Vue.use(Vuetify)
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('AppHome', require('./components/AppHome.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -32,6 +32,9 @@ Vue.use(Vuetify)
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+import router from './router/router.js'
+
 const app = new Vue({
     el: '#app',
+    router
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,36 +3,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title></title>
+    <title>Single Page Forum</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
     <div id="app">
         <v-app>
-            <v-toolbar dark color="primary">
-                <v-toolbar-side-icon></v-toolbar-side-icon>
-
-                <v-toolbar-title class="white--text">Title</v-toolbar-title>
-
-                <v-spacer></v-spacer>
-
-                <v-btn icon>
-                    <v-icon>search</v-icon>
-                </v-btn>
-
-                <v-btn icon>
-                    <v-icon>apps</v-icon>
-                </v-btn>
-
-                <v-btn icon>
-                    <v-icon>refresh</v-icon>
-                </v-btn>
-
-                <v-btn icon>
-                    <v-icon>more_vert</v-icon>
-                </v-btn>
-            </v-toolbar>
+            <app-home></app-home>
         </v-app>
     </div>
 <script src="{{asset('js/app.js')}}" ></script>
