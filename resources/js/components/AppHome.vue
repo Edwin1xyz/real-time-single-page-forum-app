a2b5b55cc6e55eab11db2ced3def471bf6c1f803
Edwin_Eddie <idongesitedidiong123@gmail.com>
2019-07-09 01:07:43 +0100

    Updating

--- resources/js/components/AppHome.vue
+++ resources/js/components/AppHome.vue
@@ -2,14 +2,16 @@
     <div>
         <toolbar></toolbar>
         <app-footer></app-footer>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
     import toolbar from './Toolbar'
     import AppFooter from './AppFooter'
+    import Login from './login/Login'
     export default {
-        components:{toolbar,AppFooter},
+        components:{toolbar,AppFooter,Login},
     }
 </script>
 
---
+++ resources/js/components/login/Login.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-form v-model="valid">
+        <v-container>
+            <v-layout>
+
+                <v-flex
+                        xs12
+                        md4
+                >
+                    <v-text-field
+                            v-model="form.email"
+                            :type="email"
+                            label="E-mail"
+                            required
+                    ></v-text-field>
+                </v-flex>
+
+                <v-flex
+                        xs12
+                        md4
+                >
+                    <v-text-field
+                            v-model="form.password"
+                            :type="password"
+                            :counter="10"
+                            label="Password"
+                            required
+                    ></v-text-field>
+                </v-flex>
+
+                <v-btn
+                        color="green"
+                        type="submit"
+                >Login</v-btn>
+
+            </v-layout>
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                form: {
+                    email:null,
+                    password:null
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
